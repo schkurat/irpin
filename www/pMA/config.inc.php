d2edd2bf55fc1be13bf57ad7f65326a68ece69ba
onzar <60158177+onzar@users.noreply.github.com>
2020-01-23 15:33:11 +0200

    phpMyAdmin

---
+++ www/pMA/config.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$i=0;
+$i++;
+$cfg['Servers'][$i]['host']          = 'localhost';
+$cfg['Servers'][$i]['extension']     = 'mysql';
+$cfg['Servers'][$i]['user']          = 'root';
+$cfg['Servers'][$i]['port']          = '3306';
+$cfg['Servers'][$i]['connect_type']  = 'tcp';
+$cfg['Servers'][$i]['password']      = 'a4949515n248s'; // use here your password
+$cfg['Servers'][$i]['auth_type']     = 'http';
+$cfg['blowfish_secret'] = 'andrey';
+?>
