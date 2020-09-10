9ea19d6869c44f4f44414a763ec979ae70408339
Andrey <schkurat.andrey@gmail.com>
2020-09-10 23:10:45 +0300

    Add the ability to search for an order by address or customer name in the accounting block

--- www/arhiv/oplata_info.php
+++ www/arhiv/oplata_info.php
@@ -3,56 +3,78 @@ include("../function.php");
 
 ?>
 <script type="text/javascript">
-$(document).ready(
-  function()
-  {
- /*$('#nzam').bind('blur',net_fokusa);*/
- $('#nzam').keyup(net_fokusa);
- function net_fokusa(eventObj){
-	$.ajax({
-		type: "POST",
-		url: "seach_zm.php",
-		data: 'nzam='+ $("#nzam").val()+'&szam='+$("#szam").val(),
-		dataType: "html",
-		success: function(html){
-			var reply=html;
-		$("#vstavka").empty();
-		$("#vstavka").append(reply); 
-		},
-		error: function(html){alert(html.error);}
-	});								
-	}
-  } 
-);
+    $(document).ready(
+        function () {
+            /*$('#nzam').bind('blur',net_fokusa);*/
+            $('#nzam, #address, #pib').keyup(net_fokusa);
+
+            function net_fokusa(eventObj) {
+                $.ajax({
+                    type: "POST",
+                    url: "seach_zm.php",
+                    data: 'nzam=' + $("#nzam").val() + '&szam=' + $("#szam").val() + '&address=' + $("#address").val() + '&pib=' + $("#pib").val(),
+                    dataType: "html",
+                    success: function (html) {
+                        var reply = html;
+                        $("#vstavka").empty();
+                        $("#vstavka").append(reply);
+                    },
+                    error: function (html) {
+                        alert(html.error);
+                    }
+                });
+            }
+        }
+    );
 </script>
 <form action="arhiv.php" name="myform" method="get">
-<table align="center" id="vstavka" class="zmview"></table>
-<br>
-<table align="center" class="zmview">
-<tr bgcolor="#B5B5B5"><th  align="center" colspan="2">Ручна розноска банка</th></tr>
-<tr>
-<td>Серія замовлення
-</td>
-<td style="width:100px"><input name="szam" id="szam" type="text" size="6" maxlength="6" value="<?php echo date("dmy");?>"/></td>
-</tr>
-<tr>
-<td>Номер замовлення</td>
-<td style="width:100px"><input name="nzam" id="nzam" type="text" size="6" maxlength="6" required />
-</td></tr>
-<tr>
-<td>Дата оплати</td>
-<td style="width:100px"><input class="datepicker" name="dopl" type="text" required />
-</td></tr>
-<tr><td>Сума</td>
-<td><input name="sum" type="text" value=""></td></tr>
-<tr><td>Комісія банка</td>
-<td><input name="kom" type="text" value="0"></td></tr>
+    <table align="center" id="vstavka" class="zmview"></table>
+    <br>
+    <table align="center" class="zmview">
+        <tr bgcolor="#B5B5B5">
+            <th align="center" colspan="2">Ручна розноска банка</th>
+        </tr>
+        <tr>
+            <td>Серія замовлення
+            </td>
+            <td style="width:100px"><input name="szam" id="szam" type="text" size="6" maxlength="6"
+                                           value="<?php echo date("dmy"); ?>"/></td>
+        </tr>
+        <tr>
+            <td>Номер замовлення</td>
+            <td style="width:100px"><input name="nzam" id="nzam" type="text" size="6" maxlength="6" required/>
+            </td>
+        </tr>
+        <tr>
+            <td>ПІБ (Назва)</td>
+            <td style="width:100px"><input name="pib" id="pib" type="text"/>
+            </td>
+        </tr>
+        <tr>
+            <td>Вулиця</td>
+            <td style="width:100px"><input name="address" id="address" type="text"/>
+            </td>
+        </tr>
+        <tr>
+            <td>Дата оплати</td>
+            <td style="width:100px"><input class="datepicker" name="dopl" type="text" required/>
+            </td>
+        </tr>
+        <tr>
+            <td>Сума</td>
+            <td><input name="sum" type="text" value=""></td>
+        </tr>
+        <tr>
+            <td>Комісія банка</td>
+            <td><input name="kom" type="text" value="0"></td>
+        </tr>
 
-<tr bgcolor="#FFFAF0"><td align="center" colspan="2">
-<input name="filter" type="hidden" value="oplata_add">
-<input name="Ok" type="submit" value="Внести" />
+        <tr bgcolor="#FFFAF0">
+            <td align="center" colspan="2">
+                <input name="filter" type="hidden" value="oplata_add">
+                <input name="Ok" type="submit" value="Внести"/>
 </form>
-<a href="arhiv.php?filter=logo" ><input name="Cancel" type="button" value="Відміна" /></a>
+<a href="arhiv.php?filter=logo"><input name="Cancel" type="button" value="Відміна"/></a>
 </td>
 </tr>
 </table>
--- www/arhiv/seach_zm.php
+++ www/arhiv/seach_zm.php
@@ -14,22 +14,30 @@ if (!@mysql_select_db(kpbti, $db)) {
     exit();
 }
 
-if (isset($_POST['szam']) and isset($_POST['nzam'])) {
+if ((!empty($_POST['szam']) and !empty($_POST['nzam'])) OR (!empty($_POST['pib']) AND mb_strlen($_POST['pib']) > 2)) {
     $szam = $_POST['szam'];
     $nzam = $_POST['nzam'];
+    $pib = $_POST['pib'];
 
-
-    if ($szam != "" and $nzam != "") {
+    if (!empty($pib)) {
+        $ath = mysql_query("SELECT arhiv_zakaz.* FROM arhiv_zakaz  
+	WHERE (LOCATE('$pib',arhiv_zakaz.SUBJ)!=0 OR LOCATE('$pib',arhiv_zakaz.PR)!=0 OR LOCATE('$pib',arhiv_zakaz.IM)!=0) 
+	OR LOCATE('$pib',arhiv_zakaz.PB)!=0 AND arhiv_zakaz.DL='1';");
+    } else {
         $ath = mysql_query("SELECT arhiv_zakaz.* FROM arhiv_zakaz  
 	WHERE arhiv_zakaz.SZ='$szam' AND arhiv_zakaz.NZ='$nzam' AND arhiv_zakaz.DL='1';");
-        if ($ath) {
+    }
+    if ($ath) {
+        if (mysql_num_rows($ath) == 0) {
+            $p .= '<tr><td colspan="5">Замовлення не знайдено</td></tr>';
+        } else {
             while ($aut = mysql_fetch_array($ath)) {
-                $subj = htmlspecialchars($aut["SUBJ"], ENT_QUOTES);
+                $subj = (!empty($aut["SUBJ"])) ? htmlspecialchars($aut["SUBJ"], ENT_QUOTES) : $aut["PR"] . ' ' . $aut["IM"] . ' ' . $aut["PB"];
                 $fio = $aut["PR"] . " " . $aut["IM"] . " " . $aut["PB"];
                 $address = '';
                 $sum = $aut["SUM"];
                 //$sumd=$aut["SUM_D"];
-                $zak = $szam . "/" . $nzam;
+                $zak = $aut["SZ"] . "/" . $aut["NZ"];
                 $zm_id = $aut["KEY"];
 
                 $sql1 = "SELECT arhiv_dop_adr.*, arhiv_dop_adr.id AS ID, rayonu.RAYON,nas_punktu.NSP,tup_nsp.TIP_NSP,
@@ -56,17 +64,40 @@ if (isset($_POST['szam']) and isset($_POST['nzam'])) {
 
                 $p .= '<tr><td>' . $zak . '</td><td>' . $subj . '</td><td>' . $address . '</td><td>' . $sum . '</td></tr>';
             }
-            mysql_free_result($ath);
         }
-    } else
-        $p .= '<tr><td colspan="5">Замовлення не знайдено</td></tr>';
-    //echo 'Текст:вася:петя';
+        mysql_free_result($ath);
+    }
     if (mysql_close($db)) {
     } else {
         echo("Не можливо виконати закриття бази");
     }
+} elseif (isset($_POST['address']) and !empty($_POST['address']) and mb_strlen($_POST['address']) > 2) {
+    $adr_key = $_POST['address'];
+    $sql1 = "SELECT arhiv_dop_adr.*, arhiv_dop_adr.id AS ID, rayonu.RAYON,nas_punktu.NSP,tup_nsp.TIP_NSP,
+			vulutsi.VUL,tup_vul.TIP_VUL,arhiv_zakaz.SZ,arhiv_zakaz.NZ,arhiv_zakaz.SUBJ,arhiv_zakaz.PR,arhiv_zakaz.IM,arhiv_zakaz.PB,arhiv_zakaz.SUM   
+			 FROM arhiv_dop_adr, rayonu, nas_punktu, vulutsi, tup_nsp, tup_vul, arhiv_zakaz
+				WHERE 
+					LOCATE('$adr_key',vulutsi.VUL)!=0 AND
+					arhiv_zakaz.KEY=arhiv_dop_adr.id_zm 
+					AND rayonu.ID_RAYONA=arhiv_dop_adr.rn
+					AND nas_punktu.ID_NSP=arhiv_dop_adr.ns
+					AND vulutsi.ID_VUL=arhiv_dop_adr.vl
+					AND tup_nsp.ID_TIP_NSP=nas_punktu.ID_TIP_NSP
+					AND tup_vul.ID_TIP_VUL=vulutsi.ID_TIP_VUL
+					ORDER BY arhiv_dop_adr.id DESC";
+    $atu1 = mysql_query($sql1);
+    while ($aut1 = mysql_fetch_array($atu1)) {
+        $zak = $aut1["SZ"] . "/" . $aut1["NZ"];
+        $subj = (!empty($aut1["SUBJ"])) ? htmlspecialchars($aut1["SUBJ"], ENT_QUOTES) : $aut1["PR"] . ' ' . $aut1["IM"] . ' ' . $aut1["PB"];
+        $bd = trim($aut1["bud"]);
+        $kv = trim($aut1["kvar"]);
+        $obj_ner_dop = objekt_ner(0, $bd, $kv);
+        $address = $aut1["TIP_NSP"] . $aut1["NSP"] . " " . $aut1["TIP_VUL"] . $aut1["VUL"] . " " . $obj_ner_dop;
+        $sum = $aut1["SUM"];
+        $p .= '<tr><td>' . $zak . '</td><td>' . $subj . '</td><td>' . $address . '</td><td>' . $sum . '</td></tr>';
+    }
+    mysql_free_result($atu1);
 } else {
     $p .= '<tr><td colspan="5">Поле номер замовлення пусте</td></tr>';
 }
 echo $p;
-?>
